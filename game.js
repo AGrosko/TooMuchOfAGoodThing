3b9f849b85bf6fdaef25ae62f63ab0fb98e2ccee
AGrosko <131299398+AGrosko@users.noreply.github.com>
2024-07-30 08:51:05 -0400

    Update game.js

---
+++ game.js
@@ -0,0 +1,19 @@
+var config = {
+    width: 800,
+    height: 450,
+    backgroundColor: 0x000000,
+    scene: [PlayScene,Playing],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    }
+  }
+  var gameSettings = {
+    playerSpeed: 200,
+  }
+  
+  
+  var game = new Phaser.Game(config);
